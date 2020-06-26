5ab3e79ecc7eb61544e356c143586c7579971e62
Josiah Augustine <josiah.augustine.o@gmail.com>
2020-06-26 20:33:22 +0100

    Bug Fix: add database config for flask-mongoengine
    fix issue #7

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os.path
 import secrets
 import connexion
+
+from flask_mongoengine import MongoEngine
 # from flask_marshmallow import Marshmallow  # for validating db models; don't know if this works with mongo
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -12,4 +14,11 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 # initialize objects
+db = MongoEngine(app)
 # ma = Marshmallow(app)
+
+# mongodb database config
+app.config["MONGODB_SETTINGS"] = {
+    "host": "mongodb+srv://austino:hngi7task4@cluster0-qht2p.mongodb.net/<MyCustomer>?retryWrites=true&w=majority",
+    "connect": False
+}
