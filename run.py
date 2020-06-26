c736724869ffd76ef7d5d074bd51d4fd395a0582
chima <chimaataman@gmail.com>
2020-06-26 16:15:44 +0100

    remove run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from config import connex_app
-from config import base_dir
-
-connex_app.add_api(os.path.join(base_dir, 'api_specification.yaml'))
-
-if __name__ == '__main__':
-    connex_app.run(debug=True, port=5207)
-
