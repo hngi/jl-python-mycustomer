a58f278d3f2f72e2e33863ed36481ce1cd63e478
iameo <okwudike.emmanuel@gmail.com>
2020-06-28 17:25:21 +0100

    resolved double json parsing

--- endpoints/user/list.py
+++ endpoints/user/list.py
@@ -3,5 +3,6 @@ from models.user import User
 
 
 def get():
-    json_user_list = User.objects.all().to_json()
-    return jsonify(json_user_list)
+    # json_user_list = User.objects.all().to_json()
+    users = User.objects.all()
+    return jsonify(users)
