9862d09be02709781f1902e79e1597163092d145
KimBitrus26 <bitruschoji4real@gmail.com>
2020-06-28 19:31:38 +0100

    second commit making changes on querset

--- endpoints/store/get.py
+++ endpoints/store/get.py
@@ -4,7 +4,7 @@ from models.store import Store
 
 def getStoreById(storeId):
 
-    store = Store.objects.get(id=storeId).to_json()
+    store = Store.objects.get(id=storeId)
     if store:
         return jsonify({"Store": store}), 200
     return jsonify({"msg":"Store not found"}), 404
