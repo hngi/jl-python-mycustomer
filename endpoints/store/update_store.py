cca0e27e561096a09398fc86c0821206213a5da9
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2020-06-29 16:13:45 +0100

    Updated the update store endpoint

--- endpoints/store/update_store.py
+++ endpoints/store/update_store.py
@@ -1,14 +1,15 @@
-from flask import jsonify
+from flask import jsonify, request
 from models.store import Store
 
 
 def updateStore(storeId):
 
+    data = request.get_json()
     store = Store.objects.get(id=storeId)
     exists = storeId in Store
     store['id'] = storeId
     if exists:
-        Store[storeId].update(store)
+        Store[data].update()
     else:
         Store[storeId] = store
     return jsonify({ "status": "OK" }), 200
