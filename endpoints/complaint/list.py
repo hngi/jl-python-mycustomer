e4217840ab5adb3795ee95aaf62ded0453d357a3
onshimiye <onshimiye17@gmail.com>
2020-06-29 20:45:16 +0200

    Fix: jsonify function output

--- endpoints/complaint/list.py
+++ endpoints/complaint/list.py
@@ -1,8 +1,7 @@
 from flask import jsonify
 from models.complaint_form import Complaint
-from helpers.serializers import json_serializer
 
 
 def get():
     complaints = Complaint.objects.all()
-    return complaints
+    return jsonify(complaints)
