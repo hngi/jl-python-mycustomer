ed6e2bd259820d2a41b1fec26cc4fb4d2f1229c4
Oluwatimilehin <oluwatimilehin.id@gmail.com>
2020-06-29 12:41:30 +0100

    Updated the update user endpoint

--- endpoints/store/update_store.py
+++ endpoints/store/update_store.py
@@ -1,19 +1,14 @@
+from flask import jsonify
 from models.store import Store
-from models.user_store import UserStore
 
 
-def update(storeId):
-    """
-    Update Store instance with id=storeId from database.
-    If UserStore has object with store_ref_id=storeId, update it.
-    """
-    store = Store.objects.get_or_404(id=storeId)
-    try:
-        user_store = UserStore.objects.get(store_ref_id=storeId)
-        user_store.update()
-    except Exception as e:
-        pass
+def updateStore(storeId):
 
-    store.update()
-
-    return {"status": "OK"}, 200
+    store = Store.objects.get(id=storeId)
+    exists = storeId in Store
+    store['id'] = storeId
+    if exists:
+        Store[storeId].update(store)
+    else:
+        Store[storeId] = store
+    return jsonify({ "status": "OK" }), 200
