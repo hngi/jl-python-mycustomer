a3245ec58b7caa34a520438c13c0a6deecaedf5d
Mohammad Bilal <mohdbilal16@gmail.com>
2020-06-29 14:54:03 +0530

    Update create_customer.py

--- endpoints/customer/create_customer.py
+++ endpoints/customer/create_customer.py
@@ -8,7 +8,9 @@ def post():
     # return: JSON object
 
     data = request.get_json()
-    post_customer = Customer(**data)
-    post_customer.save()
-    output = {'id': str(post_customer.id)}
-    return jsonify({'result': output})
+    if data:
+      post_customer = Customer(**data)
+      post_customer.save()
+      output = {'id': str(post_customer.id)}
+      return jsonify({'result': output}), 201
+    return jsonify({'error_msg':"No valid data"}), 400 
