97206eb221ca98633b01edaa4097ff9d54930899
onshimiye <onshimiye17@gmail.com>
2020-06-29 10:26:57 +0200

    Fix: updated a datatype mistake in the model

--- models/complaint_form.py
+++ models/complaint_form.py
@@ -6,9 +6,9 @@ from models import user, user_store
 class Complaint(db.Document):
     user_ref_id = db.ReferenceField(user.User, required=True)
     message = db.StringField(required=True)
-    store_ref_code = db.ReferenceField(store.Store, required=True)
+    store_ref_code = db.ReferenceField(user_store.UserStore, required=True)
     status = db.StringField(default="open")
-    timestamp = db.Datetime(default=datetime.datetime.utcnow)
+    timestamp = db.DateTimeField(default=datetime.datetime.utcnow)
 
     def __repr__(self):
         return "<Complaint: from: {}; opened: {}>".format(self.store_ref_code, self.timestamp)
