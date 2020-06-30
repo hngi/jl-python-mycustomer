1d9b1e3d33377eb2e72e3845c05fe15665ef7d86
iameo <okwudike.emmanuel@gmail.com>
2020-06-30 14:03:34 +0100

    modified create file to match swagger requirements

--- endpoints/customer/create.py
+++ endpoints/customer/create.py
@@ -2,9 +2,14 @@ from flask import jsonify, request
 from models.customer import Customer
 
 def post():
+    data = request.get_json()
+    if data:
+        new_customer = {
+            'name': data['name'],
+            'phone_number': data['phone']
+        }
     try:
-        data = request.get_json()
-        new_customer = Customer(**data).save()
+        new_customer = Customer(**new_customer).save()
     except Exception as e:
         return jsonify({'error_msg':str(e)}), 405
     return jsonify({'result': new_customer}), 201
