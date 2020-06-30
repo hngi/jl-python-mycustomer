04cd4c3ff49c13bb48aa2f2d57781b272ebd8b26
iameo <okwudike.emmanuel@gmail.com>
2020-06-30 14:04:25 +0100

    modified create file to match swagger requirements II

--- endpoints/customer/create.py
+++ endpoints/customer/create.py
@@ -11,5 +11,5 @@ def post():
     try:
         new_customer = Customer(**new_customer).save()
     except Exception as e:
-        return jsonify({'error_msg':str(e)}), 405
+        return jsonify({'error_msg':"Invalid input"}), 405
     return jsonify({'result': new_customer}), 201
