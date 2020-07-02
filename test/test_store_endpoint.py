9e456148eb2b3a20955d75bd24c0239a450b97eb
chima <chimaataman@gmail.com>
2020-07-02 06:10:21 +0100

    improve tests for /store

--- test/test_store_endpoint.py
+++ test/test_store_endpoint.py
@@ -1,60 +1,55 @@
+import random
 from unittest import TestCase
 import unittest
 import requests
 import json
 
-class Test_For_Store_Endpoint(TestCase):
+
+class TestForStoreEndpoint(TestCase):
 
     def test_store_res(self):
         response = requests.get('http://api-mycustomer-python.herokuapp.com/store/all')
         self.assertEqual(response.status_code, 200)
 
     def test_store_new_no_entry(self):
-
-    	#Ensures no input means 400 Error
-        
+        # Ensures no input means 400 Error
         response = requests.post('http://api-mycustomer-python.herokuapp.com/store/new', headers={'Content-Type': 'application/json'})
         res = response.json()
         self.assertEqual(response.status_code, 400)
         self.assertEqual(res['detail'], "None is not of type 'object'")
 
     def test_store_new_valid_entry(self):
-
-    	#Ensures valid input means 201
-        #increment the digit for a new successful test run - testxxxx1 --> testxxxx2 --> and so on
-
+        # Ensures valid input means 201
+        test_instance_number = random.randint(0, 200000)
         data = {
-            'address': 'testaddr2', 
-            'name': 'testname2',
-            'email': 'testemail2', 
-            'phone': 'testphone2',  
-            'tagline': 'testtag2'
+            'phone': 'test_phone_number_' + str(test_instance_number),
+            'email': 'test_email_' + str(test_instance_number),
+            'address': 'test_address_' + str(test_instance_number),
+            'name': 'test_store_name_' + str(test_instance_number),
+            'tagline': 'test_tagline_' + str(test_instance_number)
         }
         response = requests.post('http://api-mycustomer-python.herokuapp.com/store/new', headers={'Content-Type': 'application/json'}, data=json.dumps(data))
         self.assertEqual(response.status_code, 201)
 
     def test_store_id_non_existent(self):
-
-        #Nonexistent ID SHOULD GIVE 404 - UNABLE to find store
-
+        # Nonexistent ID SHOULD GIVE 404 - UNABLE to find store
         response = requests.get('http://api-mycustomer-python.herokuapp.com/store/5efa3305114a3a878c0fa18x')
         self.assertEqual(response.status_code, 404)
 
     def test_store_new_entry_exist(self):
-
-        #Ensures existed store inputs gives store already exists message
-
+        # Ensures existed store inputs gives store already exists message
         data = {
             'phone': '8888883888',
             'email': 'strinsdsg',
             'address': 'sstrindssg',
-            'name': 'strinsdsg', 
+            'name': 'strinsdsg',
             'tagline': 'strissdng'
         }
         response = requests.post('http://api-mycustomer-python.herokuapp.com/store/new', headers={'Content-Type': 'application/json'}, data=json.dumps(data))
         res = response.json()
         self.assertEqual(response.status_code, 403)
         self.assertEqual(res['message'], "Store already exists")
+
 
 if __name__ == '__main__':
     unittest.main()
