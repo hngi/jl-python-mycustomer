7daf225c085890b9ed288d2ddab5a96c31207080
chima <chimaataman@gmail.com>
2020-07-02 05:17:32 +0100

    refactor capitalised attribute

--- models/store.py
+++ models/store.py
@@ -1,12 +1,10 @@
-
 import datetime
-
 from config import db
 
 
 class Store(db.Document):
     store_name = db.StringField(required=True, unique=True)
-    Phone_number = db.StringField(required=True)
+    phone_number = db.StringField(required=True)
     tagline = db.StringField(required=True)
     shop_address = db.StringField(required=True)
     email = db.StringField(unique=True)
