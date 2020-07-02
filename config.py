d8b9a6799cbe4d260226c776bf2dfa0a8e08162c
iameo <okwudike.emmanuel@gmail.com>
2020-07-02 15:24:11 +0100

    confidential details are now hidden in env file

--- config.py
+++ config.py
@@ -2,12 +2,21 @@ import os.path
 import connexion
 from flask_mongoengine import MongoEngine
 from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv, find_dotenv
+
+#locate and load .env file
+load_dotenv(find_dotenv())
+
 
 base_dir = os.path.abspath(os.path.dirname(__file__))
 connex_app = connexion.App(__name__, specification_dir=base_dir)
 app = connex_app.app
 
-app.config['SECRET_KEY'] = "weAreGoingToChangeThisMyCustomer"  #obviously going to change in prod. deploy 
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+
+
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 bcrypt = Bcrypt(app)
@@ -15,10 +24,11 @@ bcrypt = Bcrypt(app)
 
 # mongodb database config
 app.config["MONGODB_SETTINGS"] = {
-    "host": "mongodb://austino:hngi7task4@cluster0-shard-00-00-qht2p.mongodb.net:27017,cluster0-shard-00-01-qht2p.mongodb.net:27017,cluster0-shard-00-02-qht2p.mongodb.net:27017/MyCustomer?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-    "connect": False
+    "host": os.getenv('MONDO_DB_HOST'),
+    "connect": os.getenv('DB_CONNECT')
 }
 
 # initialize objects#
 db = MongoEngine(app)
 
+#todo: put the above in a function and return app
