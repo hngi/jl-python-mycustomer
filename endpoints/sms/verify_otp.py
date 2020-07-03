3ddd195e06ee3cf9f3f542993d8342edfbba16a0
Evans <58369673+Johnnyevans32@users.noreply.github.com>
2020-07-03 14:06:02 +0100

    Update verify_otp.py

--- endpoints/sms/verify_otp.py
+++ endpoints/sms/verify_otp.py
@@ -5,12 +5,12 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 def post(to, code):
-	account_sid = 'Your account_sid'
-	auth_token = 'Your auth_token'
+	account_sid = 'ACa22445815bbc53cde2b2f471fdd2ef46'
+	auth_token = 'e5bf88c070bdda49a6c514f0b51e79f1'
 	client = Client(account_sid, auth_token)
 	try:
 		verification_check = client.verify \
-	                           .services('Your service_sid') \
+	                           .services('VA4ff03c64c0218ad19431a8594bc923bc') \
 	                           .verification_checks \
 	                           .create(to=to, code=code)
 
