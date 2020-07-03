2b4602903cec2afa2025a235efbfb44a891fe21f
Evans <58369673+Johnnyevans32@users.noreply.github.com>
2020-07-03 13:57:33 +0100

    Update send_otp.py

--- endpoints/sms/send_otp.py
+++ endpoints/sms/send_otp.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
 def post(to,channel='sms'):
-	account_sid = 'Your account_sid'
-	auth_token = 'Your auth_token'
+	account_sid = 'ACa22445815bbc53cde2b2f471fdd2ef46'
+	auth_token = 'e5bf88c070bdda49a6c514f0b51e79f1'
 	client = Client(account_sid, auth_token)
 	try:
 		service = client.verify.services.create(friendly_name='MyCustomer')
